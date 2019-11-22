8123645a8c41da871feb9ed5683b6c72cfe6167b
hyungkwonko <hkko@hcil.snu.ac.kr>
2019-11-22 17:16:58 +0900

    simple nav added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'infovis-project';
+  showFiller = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
