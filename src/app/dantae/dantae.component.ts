44bd7a2c934c97d67292514119e423a815119161
Dantae An <48adt@naver.com>
2019-12-05 23:11:31 +0900

    score rendering with VexFlow

--- src/app/dantae/dantae.component.ts
+++ src/app/dantae/dantae.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { IMusic } from './../music';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import Vex from 'vexflow/src/index.js'
 import * as d3 from 'd3'
 
@@ -9,150 +10,159 @@ import * as d3 from 'd3'
 })
 export class DantaeComponent implements OnInit, AfterViewInit {
 
+  @Input() public data: IMusic;
   constructor() { }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    this.createScore();
+  }
 
+  ngOnChanges(changes) {
+    if (changes.data.currentValue.Filename) {
+      this.createScore();
+    }
   }
 
   createScore() {
     let VF = Vex.Flow;
 
-    /*
-    // Create an SVG renderer and attach it to the DIV element named "boo".
-    let div = document.getElementById("bbb")
+
+    // Create an SVG renderer and attach it to the DIV element named "score_view".
+    let div = document.getElementById("score_view")
     let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
     // Configure the rendering context.
-    renderer.resize(500, 200);
-    let context = renderer.getContext();
-    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
-    */
+    renderer.resize(1100, 600);
+    let ctx = renderer.getContext();
+    ctx.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+    ctx.scale(0.4, 0.4);
 
-    let vf = new VF.Factory({
-      renderer: {
-        elementId: 'bbb'
+    let registry = new VF.Registry();
+    VF.Registry.enableDefaultRegistry(registry);
+    let getNoteId = (id: string) : any => {
+      return registry.getElementById(id);
+    }
+
+    let x = 0;
+    let y = 0;
+    let width = 400;
+    let height = 0;
+    let voices = []
+    let formatter = new VF.Formatter();
+    console.log(this.data)
+    let measureCount = this.data.Score.Staves[0].length;
+
+    this.data.Score.Staves.forEach(voiceData => {
+      let voiceInMeasures = []
+      voiceData.forEach(measure => {
+        let stave = new VF.Stave(x, y, width);
+        if (measure.End_bar === 'single') {
+          stave.setEndBarType(VF.Barline.type.SINGLE);
+        }
+        else if (measure.End_bar === 'double') {
+          stave.setEndBarType(VF.Barline.type.DOUBLE);
+        }
+        else if (measure.End_bar === 'end') {
+          stave.setEndBarType(VF.Barline.type.END);
+        }
+        let notes = []
+        measure.Notations.forEach(notation => {
+          if (notation.Type === 'clef') {
+            stave.addClef(notation.Options);
+          }
+          else if (notation.Type === 'key_signature') {
+            stave.addKeySignature(notation.Options);
+          }
+          else if (notation.Type === 'time_signature') {
+            stave.addTimeSignature(notation.Options);
+          }
+          else if (notation.Type === 'set_tempo') {
+            //stave.setTempo(notation.Options, 0);
+            // TODO preprocessing 'Options': { duration: '4', bpm: 80 }
+          }
+          else if (notation.Type === 'note') {
+            let note = new VF.StaveNote(notation.Options);
+            note.setAttribute('id', notation.Element_id);
+            let str = notation.Options.duration;
+            for (let i = 0; i < str.length; i++) {
+              if (str[i] === "d") note.addDotToAll();
+            }
+            notes.push(note);
+          }
+          else if (notation.Type === 'rest') {
+            let note = new VF.StaveNote(notation.Options);
+            let str = notation.Options.duration;
+            for (let i = 0; i < str.length; i++) {
+              if (str[i] === "d") note.addDotToAll();
+            }
+            notes.push(note);
+          }
+        });
+        stave.setContext(ctx).draw();
+        
+        let voice = new VF.Voice();
+        VF.Accidental.applyAccidentals([voice], measure.Key_signature);
+        voice.setStrict(false).addTickables(notes).setStave(stave);
+
+        formatter.joinVoices([voice]).format([voice]);
+        voiceInMeasures.push([voice, stave, x]);
+        height = Math.max(voice.getBoundingBox().getH() * 1.5, height);
+
+        /*
+        //width = formatter.preCalculateMinTotalWidth([voice]) * 1.4;
+        console.log(height + ' / ' + width + ', ' + (voice.getBoundingBox().getW() * 1.4))
+
+        stave.setWidth(width);
+        voice.setStave(stave);
+        stave.setContext(ctx).draw();
+
+        formatter = new VF.Formatter().joinVoices([voice]).format([voice]);
+        */
+
+        x += width;
+      });
+      y += height;
+      height = 0;
+      x = 0;
+      voices.push(voiceInMeasures);
+    });
+    x = width * measureCount;
+    // Now x and y is the total size of score graphic.
+
+    for (let i = 0; i < voices[0].length; i++) {
+      let f = [];
+      let s = [];
+      voices.forEach(v => {
+        f.push(v[i][0]);
+        s.push([v[i][1], v[i][2]]);
+      });
+
+      let getNoteStartX = (stave: any) : number => {
+        return stave[0].getNoteStartX();
       }
+
+      let startX = Math.max(...s.map(getNoteStartX));
+      s.forEach(stave => stave[0].setNoteStartX(startX));
+      
+      formatter.format(f, width - (startX - s[0][1]));
+      voices.forEach(v => v[i][0].setContext(ctx).draw());
+    }
+
+    let ties = [];
+
+    this.data.Score.Ties.forEach(tie => {
+      ties.push(new VF.StaveTie({
+        first_note: getNoteId(tie.from),
+        last_note: getNoteId(tie.to),
+        first_indices: tie.first_indices,
+        last_indices: tie.last_indices
+      }));
     });
-    let ctx = vf.getContext();
-    // Create a stave of width 400 at position 10, 40 on the canvas.
-    //let stave = new VF.Stave(10, 40, 400);
-    let stave = vf.Stave();
+    ties.forEach(function(t) {t.setContext(ctx).draw()});
 
-    // Add a clef and time signature.
-    stave.addClef("treble").addTimeSignature("4/4");
-    /*
-    // Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
-    */
-
-    let notes = [
-      new VF.StaveNote({ keys: ["e##/5"], duration: "8d" }).
-        addAccidental(0, new VF.Accidental("##")).addDotToAll(),
-      new VF.StaveNote({ keys: ["b/4"], duration: "16" }).
-        addAccidental(0, new VF.Accidental("b")),
-      new VF.StaveNote({ keys: ["c/4"], duration: "8" }),
-      new VF.StaveNote({ keys: ["d/4"], duration: "16" }),
-      new VF.StaveNote({ keys: ["d/4"], duration: "16" }),
-      new VF.StaveNote({ keys: ["d/4"], duration: "q" }),
-      new VF.StaveNote({ keys: ["d/4"], duration: "q" }),
-      new VF.StaveNote({ keys: ["d/4"], duration: "q" }),
-      new VF.StaveNote({ keys: ["eb/4"], duration: "q" }),
-      new VF.StaveNote({ keys: ["f/4"], duration: "q" }),
-      new VF.StaveNote({ keys: ["g#/4"], duration: "h" }),
-      new VF.BarNote({ type: 'single'})
-    ];
-
-    /*
-    let beams = VF.Beam.generateBeams(notes);
-    VF.Formatter.FormatAndDraw(context, stave, notes);
-    beams.forEach(function(b) {b.setContext(context).draw()})
-
-    let ties = [
-      new VF.StaveTie({
-        first_note: notes[4],
-        last_note: notes[5],
-        first_indices: [0],
-        last_indices: [0]
-      }),
-      new VF.StaveTie({
-        first_note: notes[5],
-        last_note: notes[6],
-        first_indices: [0],
-        last_indices: [0]
-      })
-    ];
-    ties.forEach(function(t) {t.setContext(context).draw()})
-    */
-    let voice = vf.Voice().addTickables(notes);
-    vf.Formatter()
-      .joinVoices([voice])
-      .formatToStave([voice], stave);
-    vf.draw();
-    /*
-    let div2 = document.getElementById("aaa");
-    let renderer2 = new VF.Renderer(div2, VF.Renderer.Backends.SVG);
-    renderer2.resize(500, 200);
-    let ctx = renderer2.getContext();
-
-    // Create a stave of width 400 at position 10, 40 on the canvas.
-    let stave2 = new VF.Stave(10, 40, 400);
-
-    // Add a clef and time signature.
-    stave2.addClef("treble").addTimeSignature("4/4");
-
-    // Connect it to the rendering context and draw!
-    stave2.setContext(ctx).draw();
-    /*
-    let notes2 = [
-      // Beam
-      { keys: ['b/4'], duration: '8', stem_direction: -1 },
-      { keys: ['b/4'], duration: '8', stem_direction: -1 },
-      { keys: ['b/4'], duration: '8', stem_direction: 1 },
-      { keys: ['b/4'], duration: '8', stem_direction: 1 },
-      { keys: ['d/6'], duration: '8', stem_direction: -1 },
-      { keys: ['c/6', 'd/6'], duration: '8', stem_direction: -1 },
-      { keys: ['d/6', 'e/6'], duration: '8', stem_direction: -1 },
-    ];
-    
-    let stave_notes = notes2.map(function(note) { return new VF.StaveNote(note); });
-    /*
-    stave_notes[0].setStemStyle({ strokeStyle: 'green' });
-    stave_notes[1].setStemStyle({ strokeStyle: 'orange' });
-    stave_notes[1].setKeyStyle(0, { fillStyle: 'chartreuse' });
-    stave_notes[2].setStyle({ fillStyle: 'tomato', strokeStyle: 'tomato' });
-    
-    stave_notes[0].setKeyStyle(0, { fillStyle: 'purple' });
-    stave_notes[4].setLedgerLineStyle({ fillStyle: 'red', strokeStyle: 'red' });
-    stave_notes[6].setFlagStyle({ fillStyle: 'orange', strokeStyle: 'orante' });
-    */
-    /*
-    let beam1 = new VF.Beam([stave_notes[0], stave_notes[1]]);
-    let beam2 = new VF.Beam([stave_notes[2], stave_notes[3]]);
-    let beam3 = new VF.Beam(stave_notes.slice(4, 6));
-    
-    beam1.setStyle({
-      fillStyle: 'blue',
-      strokeStyle: 'blue',
-    });
-    
-    beam2.setStyle({
-      shadowBlur: 20,
-      shadowColor: 'blue',
-    });
-    */
-    
-    //VF.Formatter.FormatAndDraw(ctx, stave2, stave_notes, false);
-    
-    /*
-    beam1.setContext(ctx).draw();
-    beam2.setContext(ctx).draw();
-    beam3.setContext(ctx).draw();
-    */
+    VF.Registry.disableDefaultRegistry();
   }
 
 }
